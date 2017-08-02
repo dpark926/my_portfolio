bf215358d7f1f471be71512454c4a71dc89bc086
Daniel Park <dpeace926@gmail.com>
2017-08-02 18:05:43 -0400

    added profile content

--- my_portfolio/src/components/Profile.js
+++ my_portfolio/src/components/Profile.js
@@ -3,7 +3,13 @@ import '../styles/Profile.css'
 
 function Profile() {
   return (
-    <div className="profile-main">Daniel Park</div>
+    <div className="profile-main">
+      <div className="top-padding"></div>
+      <div className="profile-inner">
+        <h1>DANIEL PARK</h1>
+        <p>FULL STACK WEB DEVELOPER WITH A KEEN INTEREST IN SOLVING DIFFICULT PROBLEMS, AND EXPERIENCE IN JAVASCRIPT, RUBY BASED PROGRAMMING, AND EVENT MANAGEMENT. MY TECHNICAL ABILITIES, COMMUNICATION SKILLS, AND TALENT FOR LOGISTICS AND PLANNING HELPS ME AND CREATIVE SOLUTIONS THROUGH CODE FOR ANY COMPANY.</p>
+      </div>
+    </div>
   )
 }
 
